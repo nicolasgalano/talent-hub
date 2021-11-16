88c8add457d40121876069001bfcda451d1e7cd7
Nicolas Galano <nico@wedoweb.co>
2021-11-16 11:03:26 -0300

    7-subscription-confirmation

--- src/components/common/SubscriptionBox/SubscriptionBox.tsx
+++ src/components/common/SubscriptionBox/SubscriptionBox.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import { Input } from 'semantic-ui-react'
 import { Button } from 'decentraland-ui/dist/components/Button/Button';
 import { namespaces } from '../../../i18n/i18n.constants';
 import Typography from '../Typography/Typography';
@@ -24,11 +24,18 @@ const SubscriptionBox:FC<SubscriptionBoxProps> = ({imgSrc, title, description, p
           <img src={imgSrc} alt={title} />
         </div>
         <div className="info">
-          <Typography variant="h6" element="h4" className="title">{title}</Typography>
-          <Typography variant="body2" element="p" className="description">{description}</Typography>
-          <Button primary size="large">
-            {buttonText}
-          </Button>
+          <div className="info-cont">
+            <Typography variant="h4" element="h3" className="title">{title}</Typography>
+            <Typography variant="body1" element="p" className="description">{description}</Typography>
+            <div className="form">
+              <Input className="field" icon placeholder='mail@domain.com'>
+                <input />
+              </Input>
+              <Button primary size="large">
+                {buttonText}
+              </Button>
+            </div>
+          </div>
         </div>
     </div>
   );
