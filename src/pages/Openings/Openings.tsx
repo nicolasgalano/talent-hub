91b6657f3e0d1d1b363bbcad60f4c465bf4d3da1
andi25 <adallapozza@hotmail.com>
2021-11-23 10:52:34 -0300

    12-hero

--- src/pages/Openings/Openings.tsx
+++ src/pages/Openings/Openings.tsx
@@ -9,8 +9,7 @@ import { namespaces } from '../../i18n/i18n.constants';
 // UI Custom Component
 import CardList from '../../components/common/CardList/CardList';
 import Tabs from '../../components/common/Tabs/Tabs';
-import HeroPost from '../../components/common/HeroPost/HeroPost';
-import { openings2 } from '../../assets/illustrations';
+
 
 const Openings:FC = () => {
   const { t } = useTranslation(namespaces.common);
@@ -34,12 +33,6 @@ const Openings:FC = () => {
 
   return ( 
     <Fragment>
-      <HeroPost 
-      imgSrc={openings2}
-      title={ t("Post positions for modellers, developers, designers and more") }
-      description={ t("Build a team from scratch or find the missing talent to power your project.") }
-      to="/openings"
-      />
       <Tabs dataTabs={dataTab} />
       <div className="ui container">
         <CardList data={dataJobs} />
