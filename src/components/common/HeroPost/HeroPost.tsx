7e6ad28e48b969a3c0cdbc0bec87f9fbdca40a88
Matias <arausmatias@gmail.com>
2021-12-04 19:59:40 -0300

    feature/12-hero

--- src/components/common/HeroPost/HeroPost.tsx
+++ src/components/common/HeroPost/HeroPost.tsx
@@ -1,38 +1,42 @@
 import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
-import { namespaces } from '../../../i18n/i18n.constants';
 import Typography from '../Typography/Typography';
 import { Button } from 'decentraland-ui/dist/components/Button/Button';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon/Icon'
+import { useHistory } from 'react-router';
 
 import './HeroPost.scss';
+import clsx from 'clsx';
 
 interface HeroProps {
-  imgSrc: string;
+  imgSrc?: string;
   title: string;
   description: string;
-  to: string;
+  buttonText: string,
 }
 
-const HeroPost: FC<HeroProps> = ({ imgSrc, title, description, to }) => {
-  const { t } = useTranslation(namespaces.common);
+const HeroPost: FC<HeroProps> = ({ imgSrc, title, description, buttonText }) => {
+  const history = useHistory();
+
+  const goBack = () => history.goBack();
 
   return (
-
-    <div className="gridContent">
+    <div className={clsx('gridContent', {'withoutImg': !imgSrc})}>
       <div className="ui container">
-        <div className="heroContent">
+        <div className="heroPost">
           <div className="info">
-            <Button basic size="small">
+            <Button basic size="small" onClick={() => goBack()}>
                 <Icon name="chevron left" />
-              BACK TO OPENINGS
+                {buttonText}
             </Button>
             <Typography variant="heading-s" element="h4" className="title">{title}</Typography>
             <Typography variant="body-xl" element="p" className="description">{description}</Typography>
           </div>
-          <div className="illustration">
-            <img src={imgSrc} alt={title} />
-          </div>
+          {
+            imgSrc &&
+              <div className="illustration">
+                <img src={imgSrc} alt={title} />
+              </div>
+          }
         </div>
       </div>
     </div>
