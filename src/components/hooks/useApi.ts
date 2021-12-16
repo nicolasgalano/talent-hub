ed2537928767a5e2fa36589f1cc0543f64eab998
Matias <arausmatias@gmail.com>
2021-12-16 18:04:02 -0300

    feature/47-configurar-axios

---
+++ src/components/hooks/useApi.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosInstance, AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+// Create instance
+const axiosClient: AxiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': '*/*'
+  },
+  timeout: 30000
+});
+
+const useApi = (axiosParams: AxiosRequestConfig) => {
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async (params: AxiosRequestConfig) => {
+    try {
+      const result = await axiosClient.request(params);
+      setResponse(result);
+    } catch( err ) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const sendData = () => {
+    fetchData(axiosParams);
+  }
+
+  useEffect(() => {
+    if(axiosParams.method === "GET" || axiosParams.method === "get"){
+      fetchData(axiosParams);
+    }
+  },[]);
+
+  return { response, error, loading, sendData };
+}
+
+export default useApi;
