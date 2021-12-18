e0ad195d8d8ba0e4d2842fcea63554343ddd9148
Matias <arausmatias@gmail.com>
2021-12-17 22:55:03 -0300

    feature/59-strapi-jobs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,20 @@
-import { FC, Fragment } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+// Services
+import { formatJobs, formatProfessionals } from '../../services/formatData';
+
+// Files
 import { openings3D, professionals3D } from '../../assets/illustrations';
 import { namespaces } from '../../i18n/i18n.constants';
 import SubscriptionBox from '../../components/common/SubscriptionBox/SubscriptionBox';
-import jobs from '../../data/openings.json';
-import professionals from '../../data/professionals.json';
 import './Home.scss';
+
+// Hooks
+import useApi from '../../components/hooks/useApi';
+
+// Interface
+import { CardProps } from '../../components/common/Card/Card';
 
 // UI Custom Component
 import Card3D from '../../components/common/Card3D/Card3D';
@@ -15,6 +23,40 @@ import FeaturedCards from '../../components/common/FeaturedCards/FeaturedCards';
 
 const Home:FC = () => {
   const { t } = useTranslation(namespaces.pages.home);
+  const [jobs, setJobs] = useState<CardProps[] | null>(null);
+  const [professionals, setProfessionals] = useState<CardProps[] | null>(null);
+
+  // Get jobs
+  const { response: jobsResponse, error: jobsError, loading: jobsLoading } = useApi({
+    method: 'GET',
+    url: '/jobs'
+  });
+
+  useEffect(() => {
+    jobsError && console.log('Error on request: ', jobsError.response); // TODO: Only for testing porpuse
+
+    if(jobsResponse && !jobsError) {
+      let data = jobsResponse.data;
+      data.length && setJobs(formatJobs(data));
+      // console.log('resJobs: ', formatJobs(data));
+    }
+  }, [jobsResponse, jobsError])
+
+  // Get professionals
+  const { response: professionalsResponse, error: professionalsError, loading: professionalsLoading } = useApi({
+    method: 'GET',
+    url: '/professionals'
+  });
+
+  useEffect(() => {
+    professionalsError && console.log('Error on request: ', professionalsError.response); // TODO: Only for testing porpuse
+
+    if(professionalsResponse && !professionalsError) {
+      let data = professionalsResponse.data;
+      data.length && setProfessionals(formatProfessionals(data));
+      // console.log('resProfessionals: ', formatProfessionals(data));
+    }
+  }, [professionalsResponse, professionalsError])
 
   return (
     <Fragment>
@@ -43,21 +85,28 @@ const Home:FC = () => {
         </div>
       </div>
 
-      <div className="ui container" id={`featured-${ t("featured.jobs") }`}>
-        <FeaturedCards 
-          data={jobs}
-          title={`Featured ${ t("featured.jobs") }`}
-          action={`view all ${ t("featured.jobs") }`}
-          to="/openings" />
-      </div>
+      {
+        (!jobsLoading && !jobsError) &&
+          <div className="ui container" id={`featured-${ t("featured.jobs") }`}>
+            <FeaturedCards 
+              data={jobs}
+              title={`Featured ${ t("featured.jobs") }`}
+              action={`view all ${ t("featured.jobs") }`}
+              to="/openings" />
+          </div>
+      }
 
-      <div className="ui container" id={`featured-${ t("featured.professionals") }`}>
-        <FeaturedCards 
-          data={professionals}
-          title={`Featured ${ t("featured.professionals") }`}
-          action={`view all ${ t("featured.professionals") }`}
-          to="/professionals" />
-      </div>
+
+      {
+        (!professionalsLoading && !professionalsError) &&
+          <div className="ui container" id={`featured-${ t("featured.professionals") }`}>
+            <FeaturedCards 
+              data={professionals}
+              title={`Featured ${ t("featured.professionals") }`}
+              action={`view all ${ t("featured.professionals") }`}
+              to="/professionals" />
+          </div>
+      }
 
       <div className="ui container" id="subscription-box">
         <SubscriptionBox
