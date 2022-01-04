7f96ff70e3d1e85fde4edcad8c0886020244f043
Matias <arausmatias@gmail.com>
2022-01-04 15:34:27 -0300

    feature/56-profile-details-contact

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -89,11 +89,6 @@ const professionalsRoutes: RouteInterface = {
   exact: true,
   children: [
     {
-      path: '/professional/:slug',
-      name: 'Detail',
-      component: ProfessionalDetail
-    },
-    {
       path: '/professionals/create',
       name: 'Create a profile',
       component: ProfessionalCreate,
@@ -119,7 +114,13 @@ const professionalsRoutes: RouteInterface = {
       component: ProfessionalCreateSuccess,
     },
     {
-      path: '/professional/success',
+      path: '/professional/:slug',
+      name: 'Detail',
+      component: ProfessionalDetail,
+      exact: true,
+    },
+    {
+      path: '/professional/:slug/success',
       name: 'Success',
       component: ProfessionalContactSuccess,
     },
