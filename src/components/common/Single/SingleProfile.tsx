5a7ca71da5a3189f058a0d6301d6650ba907cf92
Matias <arausmatias@gmail.com>
2022-01-05 16:27:17 -0300

    feature/55-profile-details-endpoint

--- src/components/common/Single/SingleProfile.tsx
+++ src/components/common/Single/SingleProfile.tsx
@@ -8,6 +8,7 @@ import { namespaces } from '../../../i18n/i18n.constants';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { formatContract, formatField, formatSchedule, getMultipleField } from '../../../utils/formatData';
 
 // Custom component
 import Typography from '../Typography/Typography';
@@ -26,9 +27,9 @@ export type SingleProfileType = {
   company_project_candidate: string;
   image?: string;
   profession_job_name: string;
-  workgin_shedule?: string;
-  type_of_contract?: string;
-  fields?: Array<string>;
+  workgin_shedule?: Array<any>;
+  type_of_contract?: Array<any>;
+  fields?: Array<any>;
 }
 
 interface SingleProfileProps {
@@ -39,6 +40,27 @@ const SingleProfile:FC <SingleProfileProps> = ({data}) => {
   const { t } = useTranslation(namespaces.common);
 
   const {width} = useWindowSize();
+
+  // Schedule
+  let schedule = getMultipleField(data.workgin_shedule);
+
+  if(schedule){
+    schedule = schedule.map((val) => formatSchedule(val, t));
+  }
+
+  // Contract
+  let contract = getMultipleField(data.type_of_contract);
+
+  if(contract){
+    contract = contract.map((val) => formatContract(val, t));
+  }
+
+  // Field
+  let fields = getMultipleField(data.fields);
+
+  if(fields){
+    fields = fields.map((val) => formatField(val, t));
+  }
 
   const settings = {
     slidesToShow: 1,
@@ -51,33 +73,33 @@ const SingleProfile:FC <SingleProfileProps> = ({data}) => {
   const renderMoreInfo = () => (
     <>
       {
-        data.type_of_contract &&
+        contract &&
           <div>
             {/* Type of contract */}
             <Label type="review">{t("general.type-of-contract")}</Label>
             <Typography variant="body-l" element="p">
-              {data.type_of_contract}
+              { contract.join(' / ') }
             </Typography>
           </div>
       }
       {
-        data.workgin_shedule.length !== 0 &&
+        schedule &&
           <div>
             {/* Working schedule */}
             <Label type="review">{t("general.working-schedule")}</Label>
             <Typography variant="body-l" element="p">
-              {data.workgin_shedule}
+              { schedule.join(' / ') }
             </Typography>
           </div>
       }
       {
-        data.fields.length !== 0 &&
+        fields &&
           <div>
             {/* Field */}
             <Label type="review">Field</Label>
             <div className="fields">
             {
-              data.fields.map((field) => (
+              fields.map((field) => (
                 <Tag>{field}</Tag>
               ))
             }
